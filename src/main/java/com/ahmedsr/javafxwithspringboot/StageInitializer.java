422107149e0d66ca899d07b0818ba6c68f676d9b
Ahmed Saber <38571794+DEV-A7med@users.noreply.github.com>
2021-10-20 12:29:18 +0200

    Create Reactive Spring Boot using FXTrayIcon

--- src/main/java/com/ahmedsr/javafxwithspringboot/StageInitializer.java
+++ src/main/java/com/ahmedsr/javafxwithspringboot/StageInitializer.java
@@ -1,6 +1,7 @@
 package com.ahmedsr.javafxwithspringboot;
 
 import com.ahmedsr.javafxwithspringboot.uicontroller.SimpleController;
+import com.ahmedsr.javafxwithspringboot.utils.Constants;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
@@ -18,25 +19,17 @@ import java.net.URL;
 public class StageInitializer implements ApplicationListener<StageReadyEvent> {
     private final String applicationTitle;
     private final FxWeaver fxWeaver;
-    private final URL applicationIcon; // = AlphaSyncUiApplication.class.getClassLoader().getResource(Constants.APPLICATION_ICON_LOCATION);
 
     public StageInitializer(@Value("${spring.application.ui-title}") String applicationTitle, FxWeaver fxWeaver) {
         this.applicationTitle = applicationTitle;
         this.fxWeaver = fxWeaver;
-        this.applicationIcon = JavafxWithSpringbootApplication.class.getClassLoader().getResource("assets/icons8-link-64.png");
     }
 
 
     @Override
     public void onApplicationEvent(StageReadyEvent event) {
         Stage stage = event.getStage();
-        stage.setScene(new Scene(fxWeaver.loadView(SimpleController.class), 320, 240));
-        stage.setTitle(applicationTitle);
-        try {
-            stage.getIcons().add(new Image(applicationIcon.openStream()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        stage.show();
+        //TODO:- add FXTrayIcon code here (best practice)
+
     }
 }
---
+++ src/main/java/com/ahmedsr/javafxwithspringboot/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.ahmedsr.javafxwithspringboot.utils;
+
+import com.ahmedsr.javafxwithspringboot.JavafxWithSpringbootApplication;
+
+import java.net.URL;
+
+public final class Constants {
+    // == Fields ==
+    public static final URL APPLICATION_ICON = JavafxWithSpringbootApplication.class.getClassLoader().getResource("assets/icons8-link-64.png"); ;
+    private Constants(){
+
+    }
+}
